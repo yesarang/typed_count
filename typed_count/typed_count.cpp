4377588ec3951d214be1a5f814015e5893ab5dda
Yoonsoo Kim <yesarang.kim@gmail.com>
2019-07-04 15:12:27 -0700

    Add project files.

---
+++ typed_count/typed_count.cpp
@@ -0,0 +1,12 @@
+﻿// typed_count.cpp : Defines the entry point for the application.
+//
+
+#include "typed_count.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
